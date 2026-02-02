78d5b4717fae3e766e36be746cdce2618dbe7b97
shiori-66 <jgykpmvj@gmail.com>
2026-02-03 07:37:13 +0900

    first commit

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var favicon = require('server-favicon');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello'); 
 var notesRouter = require('./routes/notes');
 var reportRouter = require('./routes/report');
+var catRouter = require('./routes/cat');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -28,6 +31,7 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/report',reportRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
